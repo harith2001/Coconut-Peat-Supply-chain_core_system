40cab7b7548791d4459d5c53b8f24f547a8becf7
harith2001 <danula243@gmail.com>
2024-12-02 00:14:21 +0530

    Grading : total count added to DB

--- plugins/grading/grading_plugin.go
+++ plugins/grading/grading_plugin.go
@@ -102,7 +102,7 @@ func (s *GradingPluginServer) ExecutePlugin(ctx context.Context, req *proto.Plug
 	update := bson.M{
 		"$set": bson.M{
 			"process":    "completed",
-			"results":    map[string]interface{}{"qualified": qualified, "acceptable": acceptable, "rejected": 10},
+			"results":    map[string]interface{}{"total": totalCount, "qualified": qualified, "acceptable": acceptable, "rejected": 10},
 			"updated_at": time.Now(),
 		},
 	}
